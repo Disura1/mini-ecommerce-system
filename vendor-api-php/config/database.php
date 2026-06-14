8cb1d0f04e6581ae7a96740187363d01bf8a7c2a
Disura1 <disurasandaruwan0@gmail.com>
2026-06-14 14:32:02 +0530

    security: remove credentials from version control

--- vendor-api-php/config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'host'     => 'localhost',
-    'port'     => '5432',
-    'dbname'   => 'mini_ecommerce_vendor_api_db',
-    'user'     => 'postgres',
-    'password' => 'Dis29@pgsql',
-];
